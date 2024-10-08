fc091d6e7e22c200272f4a0342c558ac05028e91
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-10-09 01:45:47 +0400

    fix: composer name

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'chaiqou/kutaisiphp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '46b5032d8fc72fd7a34598c443c25ca036fbb27e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'chaiqou/kutaisiphp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '46b5032d8fc72fd7a34598c443c25ca036fbb27e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

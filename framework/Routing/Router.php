8d66f9d3d5811b5e29b759d3a08b9947184d7655
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-10-11 19:05:13 +0400

    feat: handle scenario where route not found or for route method not allowed

--- framework/Routing/Router.php
+++ framework/Routing/Router.php
@@ -3,12 +3,25 @@
 namespace Chaiqou\Framework\Routing;
 
 use Chaiqou\Framework\Http\Request;
+use FastRoute\Dispatcher;
+use FastRoute\Dispatcher\Result\Matched;
+use FastRoute\Dispatcher\Result\MethodNotAllowed;
+use FastRoute\Dispatcher\Result\NotMatched;
 use function FastRoute\simpleDispatcher;
 
 class Router implements RouterInterface
 {
 
     public function dispatch(Request $request): array
+    {
+        $routeInfo = $this->extractRouteInfo($request);
+        [$status, [$controller, $method], $variables] = $routeInfo;
+
+
+        return [[new $controller, $method], $variables];
+    }
+
+    private function extractRouteInfo(Request $request): Matched|MethodNotAllowed|NotMatched
     {
         $dispatcher = simpleDispatcher(function (\FastRoute\RouteCollector $routeCollector) {
             $routes = require BASE_PATH . '/routes/web.php';
@@ -24,9 +37,13 @@ class Router implements RouterInterface
                 $request->getPathInfo()
             );
 
-        [$status, [$controller, $method], $variables] = $routeInfo;
-
-
-        return [[new $controller, $method], $variables];
+        switch($routeInfo[0]) {
+            case Dispatcher::NOT_FOUND:
+                throw new \Exception('Route not found', 404);
+            case Dispatcher::METHOD_NOT_ALLOWED:
+                throw new \Exception('Method not allowed', 405);
+            case Dispatcher::FOUND:
+                return $routeInfo;
+        }
     }
 }

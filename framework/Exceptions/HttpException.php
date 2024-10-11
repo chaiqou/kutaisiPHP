32fc721003f573f5a2b72c52b50825e6270cdb4d
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-10-11 19:18:30 +0400

    feat: create http exception

---
+++ framework/Exceptions/HttpException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Chaiqou\Framework\Exceptions;
+
+class HttpException extends \Exception
+{
+
+}
--- framework/Routing/Router.php
+++ framework/Routing/Router.php
@@ -2,16 +2,21 @@
 
 namespace Chaiqou\Framework\Routing;
 
+use Chaiqou\Framework\Exceptions\HttpException;
 use Chaiqou\Framework\Http\Request;
 use FastRoute\Dispatcher;
 use FastRoute\Dispatcher\Result\Matched;
 use FastRoute\Dispatcher\Result\MethodNotAllowed;
 use FastRoute\Dispatcher\Result\NotMatched;
+use FastRoute\RouteCollector;
 use function FastRoute\simpleDispatcher;
 
 class Router implements RouterInterface
 {
 
+    /**
+     * @throws \Exception
+     */
     public function dispatch(Request $request): array
     {
         $routeInfo = $this->extractRouteInfo($request);
@@ -26,7 +31,7 @@ class Router implements RouterInterface
      */
     private function extractRouteInfo(Request $request): Matched|MethodNotAllowed|NotMatched
     {
-        $dispatcher = simpleDispatcher(function (\FastRoute\RouteCollector $routeCollector) {
+        $dispatcher = simpleDispatcher(function (RouteCollector $routeCollector) {
             $routes = require BASE_PATH . '/routes/web.php';
             foreach ($routes as $route) {
                 [$method, $path, $handler] = $route;
@@ -42,9 +47,9 @@ class Router implements RouterInterface
 
         switch($routeInfo[0]) {
             case Dispatcher::NOT_FOUND:
-                throw new \Exception('Route not found', 404);
+                throw new HttpException('Route not found', 404);
             case Dispatcher::METHOD_NOT_ALLOWED:
-                throw new \Exception('Method not allowed', 405);
+                throw new HttpException('Method not allowed', 405);
             case Dispatcher::FOUND:
                 return $routeInfo;
         }

d7bb20b954caf94af88f42a4808ff8ea784f9b18
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-10-11 22:43:08 +0400

    feat: register ignition for better error handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,11 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 
 use App\Core\App;
+use Spatie\Ignition\Ignition;
+
+// Register Ignition for better error handling
+Ignition::make()->register ();
+
 
 
 // This file is the entry point of the application, it will create a new instance of the App class

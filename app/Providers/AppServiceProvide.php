29f5ba4b122db45f5252c88706e794509a99767d
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-10-12 15:12:34 +0400

    feat: create app service provider

---
+++ app/Providers/AppServiceProvide.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Container\ServiceProvider\BootableServiceProviderInterface;
+use Spatie\Ignition\Ignition;
+
+class AppServiceProvide extends AbstractServiceProvider implements BootableServiceProviderInterface
+{
+
+    public function boot(): void
+    {
+        // Register Ignition for better error handling
+        // Only do this in development mode
+        Ignition::make()->register ();
+    }
+
+    public function provides(string $id): bool
+    {
+        // TODO: Implement provides() method.
+    }
+
+    public function register(): void
+    {
+        // TODO: Implement register() method.
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,15 @@
 <?php
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-
 use App\Core\App;
+use App\Providers\AppServiceProvide;
 use League\Container\Container;
-use Spatie\Ignition\Ignition;
 
 error_reporting(0);
 
-// Register Ignition for better error handling
-Ignition::make()->register ();
 
 $container = new Container();
+$container->addServiceProvider(new AppServiceProvide());
 
 
 // This file is the entry point of the application, it will create a new instance of the App class

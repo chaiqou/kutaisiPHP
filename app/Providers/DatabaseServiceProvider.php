66c7222ce037d61a7158deec8b0e2f3d7ccc5a25
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-10-15 08:59:11 +0400

    feat: Database service provider

--- app/Providers/DatabaseServiceProvider.php
+++ app/Providers/DatabaseServiceProvider.php
@@ -2,24 +2,44 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Database\DatabaseManager;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 use League\Container\ServiceProvider\BootableServiceProviderInterface;
 
 class DatabaseServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
 {
+    protected Manager $capsule;
 
     public function boot(): void
     {
-        // TODO: Implement boot() method.
+        $config = $this->container->get('config');
+        $driver = $config->get('database.driver');
+
+        $capsule = new Manager();
+
+        $capsule->addConnection($config->get("database.{$driver}"), $driver);
+
+        $capsule->bootEloquent();
+
+        $capsule->getDatabaseManager()->setDefaultConnection($driver);
+
+        $this->capsule = $capsule;
     }
 
     public function provides(string $id): bool
     {
-        // TODO: Implement provides() method.
+     $provides = [
+            DatabaseManager::class,
+        ];
+
+        return in_array($id, $provides);
     }
 
     public function register(): void
     {
-        // TODO: Implement register() method.
+        $this->getContainer()->add(DatabaseManager::class, function () {
+            return $this->capsule->getDatabaseManager();
+        })->setShared();
     }
 }

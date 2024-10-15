30f6f7eeeb22baf7d8bc8c2d2ffec27a8b1534eb
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-10-15 08:49:06 +0400

    feat: fullfill database config

--- config/database.php
+++ config/database.php
@@ -1,5 +1,31 @@
 <?php
 
 return [
-    'default' => 'mysql',
+    'driver' => env('DB_DRIVER', 'mysql'),
+
+
+    'mysql' => [
+        'driver' => 'mysql',
+        'host' => env('DB_HOST', 'localhost'),
+        'database' => env('DB_DATABASE', 'kutaisiphp'),
+        'username' => env('DB_USERNAME', 'root'),
+        'password' => env('DB_PASSWORD', ''),
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+    ],
+
+    'sqlite' => [
+        'database' => env('DB_DATABASE', 'database.sqlite'),
+    ],
+
+    'pgsql' => [
+        'host' => env('DB_HOST', 'localhost'),
+        'database' => env('DB_DATABASE', 'kutaisiphp'),
+        'username' => env('DB_USERNAME', 'root'),
+        'password' => env('DB_PASSWORD', ''),
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+    ],
+
 ];

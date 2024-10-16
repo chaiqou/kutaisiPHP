da8884b6783de14011705b862b7addf7a8bc8eaa
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-10-16 10:22:19 +0400

    chore: dashboard route

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Views\View;
+use Laminas\Diactoros\Response;
+use Psr\Http\Message\ServerRequestInterface;
+
+class DashboardController
+{
+
+    public function __construct(
+        protected View $view
+    )
+    {
+    }
+
+    public function __invoke(ServerRequestInterface $request): Response
+    {
+        $response = new Response();
+
+        $response->getBody()->write(
+            $this->view->render('dashboard')
+        );
+
+        return $response;
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Http\HomeController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use League\Route\Router;
 use Psr\Container\ContainerInterface;
 
 return static function (Router $router, ContainerInterface $container) {
 
     $router->get('/', HomeController::class);
+    $router->get('/dashboard', DashboardController::class);
 
 };

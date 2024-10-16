bed6667fd62c80847dbc5060ca877ed62e4ea4d3
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-10-16 09:44:22 +0400

    feat: create view service provider

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Container\ServiceProvider\BootableServiceProviderInterface;
+
+class ViewServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
+{
+
+    public function boot(): void
+    {
+        // TODO: Implement boot() method.
+    }
+
+    public function provides(string $id): bool
+    {
+        // TODO: Implement provides() method.
+    }
+
+    public function register(): void
+    {
+        // TODO: Implement register() method.
+    }
+}
--- config/app.php
+++ config/app.php
@@ -1,16 +1,21 @@
 <?php
 
 
+use App\Providers\AppServiceProvide;
 use App\Providers\DatabaseServiceProvider;
+use App\Providers\RequestServiceProvider;
+use App\Providers\RouteServiceProvider;
+use App\Providers\ViewServiceProvider;
 
 return [
     'name' => env('APP_NAME', 'KutaisiPHP'),
     'debug' => env('APP_DEBUG', false),
 
     'providers' => [
-        App\Providers\AppServiceProvide::class,
-        App\Providers\RequestServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        AppServiceProvide::class,
+        RequestServiceProvider::class,
+        RouteServiceProvider::class,
         DatabaseServiceProvider::class,
+        ViewServiceProvider::class,
     ],
 ];

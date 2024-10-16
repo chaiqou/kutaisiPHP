0b3a7389d3c26dcbc6c754805d394de18301f9a9
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-10-16 09:59:28 +0400

    feat: create render method for twig

--- app/Http/HomeController.php
+++ app/Http/HomeController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http;
 
+use App\Views\View;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\ServerRequestInterface;
 
 class HomeController
 {
 
+    public function __construct(
+        protected View $view
+    )
+    {
+    }
+
     public function __invoke(ServerRequestInterface $request): Response
     {
         $response = new Response();
 
-        $response->getBody()->write($request->getUri()->getPath());
+        $response->getBody()->write(
+            $this->view->render('view')
+        );
 
         return $response;
     }
--- app/Views/View.php
+++ app/Views/View.php
@@ -2,7 +2,19 @@
 
 namespace App\Views;
 
+use Twig\Environment;
+
 class View
 {
+    public function __construct(
+        protected Environment $twig
+    )
+    {
+    }
+
+    public function render(string $view, array $data = []): string
+    {
+        return $this->twig->render($view . '.twig', $data);
+    }
 
 }

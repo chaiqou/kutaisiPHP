1c606978841cb20663634a0a53cae81cfba97a23
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-10-17 09:53:05 +0400

    chore: add register page

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Views\View;
+use Laminas\Diactoros\Response;
+use Psr\Http\Message\ServerRequestInterface;
+
+class RegisterController
+{
+    public function __construct(
+        protected View $view
+    )
+    {
+    }
+
+    public function index(ServerRequestInterface $request): Response
+    {
+        $response = new Response();
+
+        $response->getBody()->write(
+            $this->view->render('register')
+        );
+
+        return $response;
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UsersController;
@@ -11,5 +12,6 @@ return static function (Router $router, ContainerInterface $container) {
     $router->get('/', HomeController::class);
     $router->get('/dashboard', DashboardController::class);
     $router->get('/users/{user}', UsersController::class);
+    $router->get('/register', [RegisterController::class, 'index']);
 
 };
